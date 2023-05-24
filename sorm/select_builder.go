034d7d2e03ce1e5a20e4847f2eab079592d82b7f
sirui <srandroid@163.com>
2023-05-24 13:48:42 +0800

    修改

--- sorm/select_builder.go
+++ sorm/select_builder.go
@@ -64,7 +64,9 @@ func (m *SelectBuilder) In(column string, value interface{}) string {
 		return ""
 	}
 	v := sc.AssertSliceType(value)
+	fmt.Println("执行了in", v, len(v))
 	if v != nil {
+		fmt.Println("进入条件")
 		sql := fmt.Sprintf(" %s %s in(%s) ", m.link, column, Placeholders(len(v)))
 		m.values = append(m.values, v...)
 		if m.links {
