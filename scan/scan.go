a1646e483ec67b97012f6950ec9fe34c8bb8d4e1
sirui <srandroid@163.com>
2023-06-24 11:42:07 +0800

    优化

--- scan/scan.go
+++ scan/scan.go
@@ -2,9 +2,11 @@ package scan
 
 import (
 	"bytes"
+	"encoding/json"
 	"go/doc"
 	"go/parser"
 	"go/token"
+	"os"
 	"strings"
 )
 
@@ -13,35 +15,56 @@ import (
  */
 func ScanFunc(dir string, preFilter string) map[string]map[string]string {
 	result := make(map[string]map[string]string, 0)
-	fset := token.NewFileSet()
-	pkgs, _ := parser.ParseDir(fset, dir, nil, parser.ParseComments)
-	for _, pkg := range pkgs {
-		docPkg := doc.New(pkg, ".", doc.AllMethods)
-		for _, t := range docPkg.Types {
-			item := make(map[string]string, 0)
-			for _, method := range t.Methods {
-				doc := method.Doc
-				if preFilter != "" {
-					if strings.Contains(doc, preFilter) {
-						buf := bytes.NewBufferString(doc)
-						data := bytes.Buffer{}
-						for {
-							line, err := buf.ReadString('\n')
-							if err != nil {
-								break
-							}
-							if strings.HasPrefix(line, preFilter) {
-								data.WriteString(line)
-							}
-						}
-						item[method.Name] = data.String()
-					}
-				} else {
-					item[method.Name] = doc
-				}
-			}
-			result[t.Name] = item
+	_, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		_, err = os.Stat("sc-go-router")
+		if os.IsNotExist(err) {
+			panic("路由配置文件不存在")
 		}
+		bs, err := os.ReadFile("sc-go-router")
+		if err != nil {
+			panic("路由配置文件读取失败")
+		}
+		err = json.Unmarshal(bs, &result)
+		if err != nil {
+			panic("解析路由配置文件失败")
+		}
+	} else {
+		fset := token.NewFileSet()
+		pkgs, _ := parser.ParseDir(fset, dir, nil, parser.ParseComments)
+		for _, pkg := range pkgs {
+			docPkg := doc.New(pkg, ".", doc.AllMethods)
+			for _, t := range docPkg.Types {
+				item := make(map[string]string, 0)
+				for _, method := range t.Methods {
+					doc := method.Doc
+					if preFilter != "" {
+						if strings.Contains(doc, preFilter) {
+							buf := bytes.NewBufferString(doc)
+							data := bytes.Buffer{}
+							for {
+								line, err := buf.ReadString('\n')
+								if err != nil {
+									break
+								}
+								if strings.HasPrefix(line, preFilter) {
+									data.WriteString(line)
+								}
+							}
+							item[method.Name] = data.String()
+						}
+					} else {
+						item[method.Name] = doc
+					}
+				}
+				result[t.Name] = item
+			}
+		}
+		bs, err := json.Marshal(result)
+		if err != nil {
+			panic(err)
+		}
+		os.WriteFile("sc-go-router", bs, 0666)
 	}
 	return result
 }
