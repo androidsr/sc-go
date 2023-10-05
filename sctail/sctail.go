e6fa80fade5fcbaadded30c3ec9ed243c2cbffb8
sirui <sirui@wisesoft.net.cn>
2023-10-05 14:18:34 +0800

    修改

--- sctail/sctail.go
+++ sctail/sctail.go
@@ -58,8 +58,7 @@ func (m *MonitorFile) Start(contentHandler func(string)) error {
 			if !ok {
 				return nil
 			}
-			fmt.Println(event.Op.String(), event.Name)
-			if event.Op == fsnotify.Write {
+			if event.Op.String() == "WRITE" {
 				if fi.Size() > m.fileSize {
 					fmt.Println(event, ok)
 					newContent, err := m.readNewContent(m.fileOffset)
