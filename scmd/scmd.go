319c2df7f972a9376e924ac968f4b53f6b585277
sirui <srandroid@163.com>
2024-04-09 13:39:46 +0800

    优化

--- scmd/scmd.go
+++ scmd/scmd.go
@@ -64,7 +64,7 @@ func (m *Command) Command(shell string) error {
 			strings.Contains(strings.TrimSpace(shell), ".sh "))) || strings.Contains(shell, " && ") ||
 			strings.HasPrefix(strings.TrimSpace(shell), "mv ") || strings.HasPrefix(strings.TrimSpace(shell), "cp ") ||
 			strings.HasPrefix(strings.TrimSpace(shell), "rm ") || strings.HasPrefix(strings.TrimSpace(shell), "tar ") ||
-			strings.HasPrefix(strings.TrimSpace(shell), "unzip ") {
+			strings.HasPrefix(strings.TrimSpace(shell), "unzip ") || strings.HasPrefix(strings.TrimSpace(shell), "zip ") {
 			m.cmd = exec.Command("bash", "-c", shell)
 		} else {
 			newSh := strings.Fields(shell)
